616c2fe9c6433eac7c4f46c31133d35763cda051
Benjamin Wright <benjamin.wright@endurance.com>
2021-03-01 15:45:52 -0500

    moved 'photo' var within try block

--- src/util/util.ts
+++ src/util/util.ts
@@ -11,19 +11,18 @@ import Jimp = require('jimp');
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
   return new Promise( async (resolve, reject) => {
-    let photo: any;
     try {
-      photo = await Jimp.read(inputURL);
+      const photo = await Jimp.read(inputURL);
       const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
       await photo
       .resize(256, 256) // resize
       .quality(60) // set JPEG quality
       .greyscale() // set greyscale
-      .write(__dirname+outpath, (img: string)=>{
+      .write(__dirname+outpath, ()=>{
           resolve(__dirname+outpath);
       });
     } catch (error) {
-      reject(error)
+      reject(error);
     }
 });
 }
