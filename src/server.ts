86984b1c0bd43386733220bc6630320a5867d55f
Benjamin Wright <benjamin.wright@endurance.com>
2021-03-01 14:22:15 -0500

    added route for filtered image

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { promises } from 'dns';
 
 (async () => {
 
@@ -9,7 +10,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,13 +31,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get( "/filteredimage", async ( req, res ) => {
+    const { image_url:imageUrl } = req.query;
+    const urlRegex = new RegExp(/^(https?):\/\/[^\s$.?#].[^\s]*$/, 'g');
+    const isValidImageUrl = urlRegex.test(imageUrl);
+
+    if (!isValidImageUrl) {
+      res.status(400).send("A valid 'image_url' url parameter is required");
+    }
+
+    try {
+      let image = await filterImageFromURL(imageUrl)
+      res.sendFile(image);
+      res.on('finish', () => deleteLocalFiles([image]));
+    } catch(err) {
+      return res.status(400).send("There was an error processing your image");
+    }
+  } );
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
